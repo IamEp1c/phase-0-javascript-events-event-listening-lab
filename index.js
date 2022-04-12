fbb44b8d2b59cae595ac731d7bec3d5b2c1550a9
Waqas Anjum <Qanjum96@gmail.com>
2022-04-12 00:01:12 -0400

    done

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const input = document.querySelector('#input')
+
 function addingEventListener() {
+    input.addEventListener('click', (e) => {
+        e.preventDefault(); 
+        alert('I was clicked!');
+    });
 }
+
+addingEventListener(); 
